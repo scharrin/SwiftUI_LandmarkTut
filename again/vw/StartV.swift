9d8f6ebab5b1bef2bfdc4173a663c1848fb5b65d
ddd <dedeten2@naver.com>
2020-02-27 23:28:24 +0900

    Add one view memo files

--- again/vw/StartV.swift
+++ again/vw/StartV.swift
@@ -10,25 +10,92 @@ import SwiftUI
 
 struct StartV: View {
     
-    @EnvironmentObject private var udata: UData
-    
+    @State var newText: String = ""
+    @State var notes: [Note]
+    @State private var memoExpanded: Bool = false
+//    @Environment(\EditMode) var editMod
+    let newTextFrameHeight: CGFloat = 100
+    let listTextFrameHeight: CGFloat = 50
+    let listTextFrameWidth: CGFloat = 404
+    let listTextFrameSpacing: CGFloat = 5
+    let rowCornerRadius: CGFloat = 10
+    let listPadding: CGFloat = 5
+    let listTextPadding: CGFloat = 5
+    let longPressGesTime: Double = 2.0
+
     var body: some View {
-        Text("Doin somethin")
-//        NavigationView {
-//            List {
-//                ForEach ( udata.mm ) { _memo in
-//                    NavigationLink(destination: MemoDetailV(_memo: _memo)) {
-//                        memoListRowV(_memo)
-//                    }
-//                }
-//            }
-//        }
-//    .navheader  title and compose button
+        ZStack {
+            Color.gray
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                txtVw(text: $newText)
+                    .frame( height: newTextFrameHeight )
+                    .cornerRadius(rowCornerRadius)
+                ScrollView {
+                    VStack ( spacing: listTextFrameSpacing ) {
+                        ForEach( notes ) { n in
+                            Button(action: { self.memoExpanded.toggle() }) {
+                                if self.memoExpanded {
+                                    /// when tapped a memo
+                                    MemoDetailV(_memo: "fffff")
+                                } else {
+                                    /// memo preview before tap
+                                    memoListRowV()
+                                /// shapes
+                                }
+                            }
+                            .onLongPressGesture(
+                                minimumDuration: self.longPressGesTime
+                                , perform: self.rwMov
+                            )
+                            .padding(self.listTextPadding)
+                            .frame(
+                                width: self.listTextFrameWidth
+                                , height: self.listTextFrameHeight
+                                , alignment: .topLeading
+                            )
+                            .background(Color.white)
+                        }
+                        .onDelete(perform: delRw)
+                        .cornerRadius(self.rowCornerRadius)
+                        Spacer()
+                    }
+                }
+            }
+            .padding(listPadding)
+        }
+    }
+    
+    func rwMov() {
+        
+    }
+    func delRw(at delIdx : IndexSet ) {
+        if let _idx = delIdx.first {
+            self.notes.remove(at: _idx)
+        }
     }
 }
 
 struct StartV_Previews: PreviewProvider {
     static var previews: some View {
-        StartV()
+        StartV(notes:
+            [
+                Note(saveDate: Date(), headContent: "dd", contents: "contnts sample")
+                , Note(saveDate: Date(), headContent: "dd", contents: "contnts sample")
+                , Note(saveDate: Date(), headContent: "dd", contents: "contnts sample")
+                , Note(saveDate: Date(), headContent: "dd", contents: "contnts sample")
+                , Note(saveDate: Date(), headContent: "dd", contents: "contnts sample")
+                , Note(saveDate: Date(), headContent: "dd", contents: "contnts sample")
+                , Note(saveDate: Date(), headContent: "dd", contents: "contnts sample")
+                , Note(saveDate: Date(), headContent: "dd", contents: "contnts sample")
+                , Note(saveDate: Date(), headContent: "dd", contents: "contnts sample")
+                , Note(saveDate: Date(), headContent: "dd", contents: "contnts sample")
+                , Note(saveDate: Date(), headContent: "dd", contents: "contnts sample")
+                , Note(saveDate: Date(), headContent: "dd", contents: "contnts sample")
+                , Note(saveDate: Date(), headContent: "dd", contents: "contnts sample")
+                , Note(saveDate: Date(), headContent: "dd", contents: "contnts sample")
+            ]
+        )
+//        StartV({})
     }
 }
