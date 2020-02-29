590588d47a11da02b9bdafe83f9708ec5b439a54
ddd <dedeten2@naver.com>
2020-03-01 01:26:22 +0900

    Modify list row view

--- again/vw/MemoDetailV.swift
+++ again/vw/MemoDetailV.swift
@@ -10,24 +10,40 @@ import SwiftUI
 
 struct MemoDetailV: View {
     
-    @Environment(\.presentationMode) private var presentationMode
-//    @Binding var _memo =
-    var _memo: String
+//    @Environment(\.presentationMode) private var presentationMode
+    @Binding var _memo: String
+//    var _memo: String
+    private let hdrTxtLen = 13
+    private let hdrFontSize: CGFloat = 30
+    private let cntntsFontColor = Color(hue:180/360, saturation:0/100, brightness:50/100)
+
     var body: some View {
-        VStack {
-            HStack {
-                Spacer()
-                Button("save") {
-                    self.presentationMode.wrappedValue.dismiss()
+        HStack{
+            VStack (alignment: .leading){
+                if _memo.count > hdrTxtLen {
+                    Text(_memo.prefix(hdrTxtLen))
+                        .font(.system(size: hdrFontSize))
+                        .font(.headline)
+                    Text(_memo.suffix(_memo.count - hdrTxtLen))
+                        .foregroundColor(cntntsFontColor)
+                        .font(.subheadline)
+                        .lineLimit(2)
+                } else {
+                    Text(_memo)
+                        .font(.system(size: hdrFontSize))
+                        .font(.headline)
                 }
+//            Button("save") {
+//                self.presentationMode.wrappedValue.dismiss()
+//            }
             }
-//            txtVws(txt: _memo)
+        Spacer()
         }
     }
 }
 
 struct MemoDetailV_Previews: PreviewProvider {
     static var previews: some View {
-        MemoDetailV(_memo:"dddd")
+        MemoDetailV(_memo: .constant("12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"))
     }
 }
