8fb821efb1c4bf78e6cb0bf7fa9e9b378fcebfb6
ddd <dedeten2@naver.com>
2020-03-06 01:22:24 +0900

    Modify new notes action : multiline textview resizing Add samples : about geometry reader

--- again/vw/MemoDetailV.swift
+++ again/vw/MemoDetailV.swift
@@ -9,41 +9,14 @@
 import SwiftUI
 
 struct MemoDetailV: View {
-    
-//    @Environment(\.presentationMode) private var presentationMode
-    @Binding var _memo: String
-//    var _memo: String
-    private let hdrTxtLen = 13
-    private let hdrFontSize: CGFloat = 30
-    private let cntntsFontColor = Color(hue:180/360, saturation:0/100, brightness:50/100)
 
     var body: some View {
-        HStack{
-            VStack (alignment: .leading){
-                if _memo.count > hdrTxtLen {
-                    Text(_memo.prefix(hdrTxtLen))
-                        .font(.system(size: hdrFontSize))
-                        .font(.headline)
-                    Text(_memo.suffix(_memo.count - hdrTxtLen))
-                        .foregroundColor(cntntsFontColor)
-                        .font(.subheadline)
-                        .lineLimit(2)
-                } else {
-                    Text(_memo)
-                        .font(.system(size: hdrFontSize))
-                        .font(.headline)
-                }
-//            Button("save") {
-//                self.presentationMode.wrappedValue.dismiss()
-//            }
-            }
-        Spacer()
-        }
+        Text("this is Memo detail view")
     }
 }
 
 struct MemoDetailV_Previews: PreviewProvider {
     static var previews: some View {
-        MemoDetailV(_memo: .constant("12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"))
+        MemoDetailV()
     }
 }
