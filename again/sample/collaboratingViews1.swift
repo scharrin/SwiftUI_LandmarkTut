c2ee5889c7d0e8785523d72059478bff45259a04
ddd <dedeten2@naver.com>
2020-03-06 01:22:46 +0900

    Modify new notes action : multiline textview resizing Add samples : about geometry reader

---
+++ again/sample/collaboratingViews1.swift
@@ -0,0 +1,72 @@
+
+
+/// https://swiftui-lab.com/communicating-with-the-view-tree-part-1/
+///
+
+import SwiftUI
+
+struct EasyExample : View {
+    @State private var activeIdx: Int = 0
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            HStack {
+                MonthView(activeMonth: $activeIdx, label: "January", idx: 0)
+                MonthView(activeMonth: $activeIdx, label: "February", idx: 1)
+                MonthView(activeMonth: $activeIdx, label: "March", idx: 2)
+                MonthView(activeMonth: $activeIdx, label: "April", idx: 3)
+            }
+            
+            Spacer()
+            
+            HStack {
+                MonthView(activeMonth: $activeIdx, label: "May", idx: 4)
+                MonthView(activeMonth: $activeIdx, label: "June", idx: 5)
+                MonthView(activeMonth: $activeIdx, label: "July", idx: 6)
+                MonthView(activeMonth: $activeIdx, label: "August", idx: 7)
+            }
+            
+            Spacer()
+            
+            HStack {
+                MonthView(activeMonth: $activeIdx, label: "September", idx: 8)
+                MonthView(activeMonth: $activeIdx, label: "October", idx: 9)
+                MonthView(activeMonth: $activeIdx, label: "November", idx: 10)
+                MonthView(activeMonth: $activeIdx, label: "December", idx: 11)
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct MonthView: View {
+    @Binding var activeMonth: Int
+    let label: String
+    let idx: Int
+    
+    var body: some View {
+        Text(label)
+            .padding(10)
+            .onTapGesture { self.activeMonth = self.idx }
+            .background(MonthBorder(show: activeMonth == idx))
+    }
+}
+
+struct MonthBorder: View {
+    let show: Bool
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 15)
+            .stroke(lineWidth: 3.0).foregroundColor(show ? Color.red : Color.clear)
+            .animation(.easeInOut(duration: 0.6))
+    }
+}
+
+struct prv: PreviewProvider {
+    static var previews: some View {
+        EasyExample()
+    }
+}
